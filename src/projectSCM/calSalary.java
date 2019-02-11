0f9a27d915d885f887a7073f8a00585445d03cdf
puth1998 <teenaputh12@hotmail.com>
2019-02-11 23:41:45 +0700

    minor change

--- src/projectSCM/calSalary.java
+++ src/projectSCM/calSalary.java
@@ -26,7 +26,7 @@ public class calSalary {
 
 	public double sumsalary() {
 		double sumsalary = 0;
-		if( this.sales <= 50000){
+		if( this.sales <= 10000){
 			sumsalary = (int) (this.sales*0.05);
 		}else {
 			sumsalary = (int) (this.sales*0.10);
